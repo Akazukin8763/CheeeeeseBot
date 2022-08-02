f6a6fe814bebbe91c973cf6272f6988e9fd3a6f9
Akazukin8763 <starmoon900709@gmail.com>
2022-08-02 21:32:06 +0800

    feat: 更新資料庫結構 1. 不再允許其餘函式直接讀寫個別資料庫物件，一律需使用 Database 來統一獲取資料表並寫入，以降低各函式讀取資料庫所需的時間。 2. 透過 refresh 函式來獲取整體資料庫，其餘函式只需透過 get_doc, set_doc 即可更改資料表，需定時呼叫 commit 將更新結果寫入資料庫。

--- database/database.py
+++ database/database.py
@@ -1,4 +1,4 @@
-import pymongo
+from pymongo import MongoClient, UpdateOne
 from pymongo.server_api import ServerApi
 
 from twitchio.ext import commands
@@ -11,16 +11,58 @@ class Database:
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-        self.client = pymongo.MongoClient(
+        self.client = MongoClient(
             "mongodb+srv://%s:%s@%s/%s" % (Config.DB_USERNAME, Config.DB_PASSWORD, Config.DB_HOST, Config.DB_OPTIONS),
             server_api=ServerApi('1')
         )
 
+        self._channels = {}
+
     def close(self):
         self.client.close()
 
-    def __getattr__(self, name: str):
-        return self.client[name]
+    def refresh(self) -> None:
+        # 紀錄每個頻道的資料庫
+        for db in self.client.list_database_names():
+            if db == "admin" or db == "local":
+                continue
 
-    def __getitem__(self, name: str):
-        return self.client[name]
+            # 紀錄每個頻道資料庫中的每個資料表
+            data = {}
+            for coll in self.client[db].list_collection_names():
+                # 將資料表的內容轉為 dict
+                docs = {}
+                for doc in self.client[db][coll].find({}, {"_id": 0}):
+                    docs[doc["user"]] = {k: v for k, v in doc.items() if k != "user"}
+
+                data[coll] = docs
+
+            self._channels[db] = data
+
+    def commit(self) -> None:
+        # 寫入每個頻道的資料庫
+        for db in self.client.list_database_names():
+            if db == "admin" or db == "local":
+                continue
+
+            # 寫入每個頻道資料庫中的每個資料表
+            for coll in self.client[db].list_collection_names():
+                updates = []
+
+                for user, data in self._channels[db][coll].items():
+                    if data.get("update"):
+                        updates.append(UpdateOne(filter={"user": user},
+                                                 update={"$set": {k: v for k, v in data.items() if k != "update"}},
+                                                 upsert=True))
+
+                if len(updates) > 0:
+                    self.client[db][coll].bulk_write(updates)
+
+        # 將資料表重新讀出
+        self.refresh()
+
+    def get_doc(self, channel: str, doc_name: str):
+        return self._channels[channel][doc_name]
+
+    def set_doc(self, channel: str, doc_name: str, doc: dict):
+        self._channels[channel][doc_name] = doc
